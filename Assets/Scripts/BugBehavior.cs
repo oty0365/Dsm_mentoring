c3fc9c30188eed8e0c0c1603478fd8f4de7cdc87
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-03-03 23:41:27 +0900

    wip

---
+++ Assets/Scripts/BugBehavior.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BugBehavior : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public bool isAlive;
+    public Color type;
+    public float speed;
+    public string bugName;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    void InitBug()
+    {
+        isAlive = true;
+
+    }
+}
